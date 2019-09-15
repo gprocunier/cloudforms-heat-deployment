8240623cb07a2d77a0f0bfcbe90391046c836748
gprocunier <49077358+gprocunier@users.noreply.github.com>
2019-09-15 05:13:47 -0400

    Add files via upload

--- contrib/build.sh
+++ contrib/build.sh
@@ -10,27 +10,6 @@ source /home/stack/overcloud/cloudforms/cloudforms-openrc.sh
 kdestroy
 kinit admin
 
-# assumption #2 - you have ssh keys copied to your satellite from the builder host
-# assumption #3 - your sudo is configured for NOPASSWORD
-read -r -d '' WORK <<'__EOF__'
-declare -a cfme
-
-cfme=( cfme-bastion-0.idm.example.com cfme-database-0.idm.example.com  cfme-database-1.idm.example.com  cfme-portal-0.idm.example.com  cfme-portal-1.idm.example.com  cfme-worker-0.idm.example.com  cfme-worker-1.idm.example.com  cfme-worker-2.idm.example.com )
-
-for i in "${cfme[@]}"
-do
-  sudo hammer host delete --name $i >/dev/null 2>&1
-  echo -n "."
-done
-echo
-__EOF__
-
-if [[ $# -ne 1 ]]
-then
-  echo "wiping old nodes from satellite"
-  ssh gprocuni@satellite.example.com "${WORK}"
-fi
-
 echo "Hit enter to deploy cloudforms-cluster" && read
 
 # Delete pre-existing stack and entertain with a pleasant spinner
@@ -50,13 +29,22 @@ done
 printf "\b\b\nCreating...\n"
 
 # This generates the node OTP and is why we need kinit admin or equiv
-# assumption #4 - this file is downloaded out of contrib and is in the pwd of build.sh
+# assumption #2 - this file is downloaded out of contrib and is in the pwd of build.sh
 perl ./ipa_enrollment.pl
 
 openstack stack create \
-  -e 'https://raw.githubusercontent.com/gprocunier/cloudforms-heat-deployment/master/01-cloudforms-config.yaml' \
+  -e 'https://raw.githubusercontent.com/gprocunier/cloudforms-heat-deployment/raw/master/01-cloudforms-config.yaml' \
   -e 'ipa_enrollment.yaml' \
-  -t 'https://raw.githubusercontent.com/gprocunier/cloudforms-heat-deployment/master/03-cloudforms-cluster.yaml' \
+  -t 'https://raw.githubusercontent.com/gprocunier/cloudforms-heat-deployment/raw/master/03-cloudforms-cluster.yaml' \
   --wait \
-  --timeout 10 \
   cloudforms-cluster
+
+# assumption #3 - get-private-key.sh is downloaded out of contrib and in the pwd of build.sh
+if [[ ! -z $(openstack stack show cloudforms-cluster -c id -f value 2>/dev/null) ]]
+then
+  [[ -f cloudforms-server-key.rsa ]] && rm -f cloudforms-server-key.rsa
+  echo "extracting private key"
+  ./get-private-key.sh 
+  echo "copying private key to bastion"
+  scp -i ./cloudforms-server-key.rsa cloudforms-server-key.rsa cloud-user@cfme-bastion-0.idm.example.com:.
+fi
